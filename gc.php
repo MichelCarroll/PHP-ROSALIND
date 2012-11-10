9529f12b43bfc7ef89b387123b0e7f0bbe675f23
Michel Carroll <carrmi.0@gmail.com>
2012-11-09 21:11:41 -0500

    a

--- gc.php
+++ gc.php
@@ -5,7 +5,17 @@ require('common/bootstrap.php');
 
 $fasta = new FASTA(APP_DATASET);
 
-foreach($fasta->getStrandMap() as $strand)
+$resultMap = array();
+foreach($fasta->getStrandMap() as $label => $strand)
 {
-  var_dump($strand->getGCContent());
+  $resultMap[$label] = $strand->getGCContent();
 }
+
+asort($resultMap);
+
+$winnerLabel = key($resultMap);
+$winnerResult = current($resultMap);
+
+echo '>'.$winnerLabel.PHP_EOL;
+echo ($winnerResult*100).'%';
+echo PHP_EOL;
